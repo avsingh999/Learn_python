ce2b9a3821bde3ab2a0263bbafed4de2ba5479e0
Avkaran singh <avkaransingh357@gmail.com>
2017-08-25 15:18:50 +0530

    Create input_output.py

---
+++ introduction/input_output.py
@@ -0,0 +1,10 @@
+n=input("give any input: ")
+print(n)
+
+print('enter the two number')
+p1,p2=input().split()
+num1=int(p1)
+num2=int(p2)
+
+print(num1+num2,num1-num2,num1*num2,num1/num2,num1**num2,sep="\n")
+#num1**num2 -->  num1^num2
