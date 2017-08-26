26bbc9541ef3686d63452d73c88b6c86bafa1461
Avkaran singh <avkaransingh357@gmail.com>
2017-08-26 19:15:03 +0530

    Create 4loop.py

---
+++ introduction/4loop.py
@@ -0,0 +1,14 @@
+arr=[1,2,3,4,5]
+for i in range(len(arr)):
+    print(arr[i])
+
+# *** while loop*****
+print("while")
+s="pycharm"
+j=0
+while j<len(s):
+    print(s[j])
+    j+=1
+
+print("we can also print these thing")
+print(arr,s)
