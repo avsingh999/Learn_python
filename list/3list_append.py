2023ff1c18f848c4ff415320ee3d32c172ee4ef8
Avkaran singh <avkaransingh357@gmail.com>
2017-08-31 00:28:57 +0530

    Update 3list_append.py

--- list/3list_append.py
+++ list/3list_append.py
@@ -1,6 +1,13 @@
 List=[11,22,33,44,55,66,77,88]
+
+#append element 
 List.append(100)
 print(List)
 
+#add list from index
 List[3:3]=["h","e","l","o"]
+print(List)
+
+#insert elemnt at index
+List.insert(1,"hello")
 print(List)
