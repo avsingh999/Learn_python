4a3416cdca08ce1c15386f169c376975d031fc58
Avkaran singh <avkaransingh357@gmail.com>
2017-08-30 23:49:16 +0530

    Create 2list_print_etelment.py

---
+++ list/2list_print_etelment.py
@@ -0,0 +1,14 @@
+List=[11,22,33,44,55,66,77,88]
+#print total element
+print(List[0:8])
+
+#print 4 element from  start
+print(List[:4])
+
+#print 4 element from  last
+print(List[4:])
+
+#print element from last
+
+print(List[-2])
+
