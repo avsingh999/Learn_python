26aa3a78ce40dc60cdaba40d1192b468bb5b3231
Avkaran singh <avkaransingh357@gmail.com>
2017-08-30 23:30:57 +0530

    Create 1list_declaretion.py

---
+++ list/1list_declaretion.py
@@ -0,0 +1,13 @@
+#simle declaretion
+my_list=list()
+list1=[11,22,33,44]
+print("list1 =",list1)
+
+#create list with string & integer
+list2=[1,"hello",3]
+print(list2)
+
+
+#create a 2d list
+list2d=[list1,list2]
+print(list2d)
