621e16b1f8caa88206396348f37071d43249e39b
Avkaran singh <avkaransingh357@gmail.com>
2017-09-06 20:57:41 +0530

    Create 9string_stringCheck.py

---
+++ string/9string_stringCheck.py
@@ -0,0 +1,17 @@
+st="234"
+
+print(st,"->isnum: ",st.isalnum())
+
+st="Aeiou"
+print(st,"->isallalph: ",st.isalpha())
+
+st="312"
+print(st,"->chek decimle:",st.isdecimal())
+print(st,"->check digit:",st.isdigit())
+
+st="AEIou"
+print(st,"->check lower:",st.islower())
+
+print(st,"->check upper:",st.isupper())
+st="23heloo"
+print(st,"->check numeric:",st.isnumeric())
